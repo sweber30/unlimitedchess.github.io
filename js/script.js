25ed3c8fa71f7bf52e6cbc861745f382a6ace4b8
Sam Weber <sweber30@student.cvtc.edu>
2021-03-03 09:10:07 -0600

    Added move validation. Still needs castling and en passant.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+//
 // Initialize Variables
+//
+
 var isPieceSelected = false;
 //var isMovingPiece = false; // needed to support dragging pieces between squares ***************************
 var selectedSquare = '';
@@ -14,6 +28,10 @@ var pieceLocations = { // 1 = white, 2 = black. k = king, q = queen, r = rook, b
     a2:'p1', b2:'p1', c2:'p1', d2:'p1', e2:'p1', f2:'p1', g2:'p1', h2:'p1',
     a1:'r1', b1:'n1', c1:'b1', d1:'q1', e1:'k1', f1:'b1', g1:'n1', h1:'r1'
 }
+
+const columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+var moves = ""; // String to concatenate all new moves
 
 
 
@@ -72,7 +90,7 @@ function LeftClickDown(id) {
     if (isPieceSelected) { // A piece is already selected
 
         // Check if new square contains a piece of the same color.
-        if (pieceLocations[selectedSquare].substring(1, 2) == pieceLocations[id].substring(1, 2)) {
+        if (pieceLocations[selectedSquare].substr(1, 1) == pieceLocations[id].substr(1, 1)) {
 
             // Select the new piece
             selectedSquare = id;
@@ -97,7 +115,7 @@ function LeftClickDown(id) {
 
         // Check if square contains a piece that belongs to the player
         // CURRENTLY ALLOWS FOR ALL PIECES TO BE SELECTED. NEED TO ADD TURNS AND RESTRICT ONE COLOR FROM BEING SELECTED! *******************
-        if (pieceLocations[id].substring(1, 2) == 1 || pieceLocations[id].substring(1, 2) == 2) {
+        if (pieceLocations[id].substr(1, 1) == 1 || pieceLocations[id].substr(1, 1) == 2) {
 
             // Set isPieceSelected to true, and store square id.
             isPieceSelected = true;
@@ -149,25 +167,184 @@ function RightClickUp(id) {
 
 // Check if a move is valid
 function isMoveValid(oldSquare, newSquare) { // DO STUFF HERE *************************************
-    // set variables...
+    
+    // Set Variables
+    var pieceCode = pieceLocations[oldSquare];
+    var pieceMoved = pieceCode.substr(0, 1);
+    var pieceColor = pieceCode.substr(1, 1);
+    var kingLocation;
+
+    var isMoveValid = false;
+    var isPathBlocked = false;
+    var isKingInCheck = false;
+
+    var xDistance = CalculateXDistance(oldSquare, newSquare);
+    var yDistance = CalculateYDistance(oldSquare, newSquare);
+    var xDistanceAbs = Math.abs(xDistance);
+    var yDistanceAbs = Math.abs(yDistance);
+
+    var testSquarePiece = '';
+    var oppenentColor = (pieceColor == 1) ? 2 : 1;
+
+    var oldSquarePiece = pieceLocations[oldSquare];
+    var newSquarePiece = pieceLocations[newSquare];
+
+    var moveID = 0;
+
+
+    // To-Do **********************************
+    // special moves!!!!
     //
-    // piece being moved
-    // player color
-    // king location
-    // anything else?
+    // king side castle
+    // queen side castle
+    // en passant
+    // promotions
 
     //
     // If the answer to any of the following is no, return false and exit the function.
     // Otherwise, return true.
     //
 
-    // can the piece move in this way?
+    //
+    // Can the piece move in this way?
+    //
 
-    // does this move leave the player in check?
+    switch (pieceMoved) {
 
+        case "k": // King
+            isMoveValid = (xDistanceAbs <= 1 && yDistanceAbs <= 1);
+            moveID = 1;
+            break;
 
+        case "q": // Queen
+            isMoveValid = ((xDistance == 0 || yDistance == 0) || xDistanceAbs == yDistanceAbs);
+            moveID = 2;
+            break;
 
-    return true; // temporarily return true so plain movement can be tested. CHANGE THIS LATER! ************************
+        case "r": // Rook
+            isMoveValid = (xDistance == 0 || yDistance == 0);
+            moveID = 3;
+            break;
+
+        case "b": // Bishop
+            isMoveValid = (xDistanceAbs == yDistanceAbs);
+            moveID = 4;
+            break;
+
+        case "n": // Knight
+            isMoveValid = (xDistanceAbs == 1 && yDistanceAbs == 2 || xDistanceAbs == 2 && yDistanceAbs == 1);
+            moveID = 5;
+            break;
+
+        case "p": // Pawn
+            if (xDistance == 0) { // Forward pawn move
+                if (oldSquare.substr(1, 1) == ((pieceColor == 1) ? "2" : "7")) { // Pawn has not moved yet
+                    isMoveValid = (yDistance == ((pieceColor == 1) ? 1 : -1) || yDistance == ((pieceColor == 1) ? 2 : -2));
+                    moveID = 6;
+                } else { // Pawn has already moved
+                    isMoveValid = (yDistance == ((pieceColor == 1) ? 1 : -1));
+                    moveID = 7;
+                }
+            } else { // Capturing
+                if (pieceLocations[newSquare] == '  ') { // En passant capture
+                    isMoveValid == false; // ************************************************************************************
+                    moveID = 8;
+                } else { // Normal capture
+                    isMoveValid = (xDistanceAbs == 1 && (yDistance == ((pieceColor == 1) ? 1 : -1)));
+                    moveID = 9;
+                }
+            }
+            break;
+
+        default: // other unexpected values
+            console.log('isMoveValid(): Unexpected piece code in the locations object'); // TEST PURPOSES... REMOVE THIS LATER ***************************
+
+    }
+
+    //
+    // Is the path blocked by other pieces?
+    //
+
+    // Add Casting!!! **********************************************************************************************************
+    if (moveID == 2 || moveID == 3 || moveID == 4) { // queen, rook, or bishop
+        isPathBlocked = checkPathBlocked(oldSquare, newSquare, false);
+    } else if (moveID == 6 || moveID == 7) { // forward pawn move (not capturing)
+        isPathBlocked = checkPathBlocked(oldSquare, newSquare, true);
+    } else { // all other moves are a distance of one, and/or do not care if certain squares are occupied
+        isPathBlocked = false;
+    }
+
+    //
+    // Does this move (not) leave the player in check?
+    //
+
+    // Temporarily update piece location in pieceLocations object.
+    pieceLocations[newSquare] = pieceLocations[oldSquare];
+    pieceLocations[oldSquare] = '  ';
+
+    // Find king's position.
+    kingLocation = findPiece("k" + pieceColor).substr(0, 2);
+    console.log(kingLocation);
+
+    // Check horizontal, vertical, and diagonal lines out from the king for hazardous pieces.
+    for (var x = -1; x <= 1; x++) {
+        for (var y = -1; y <= 1; y++) {
+
+            testSquarePiece = checkPath(kingLocation, x, y);
+
+            if (Math.abs(x + y) == 1) { // Straight path
+                if (testSquarePiece == 'q' + oppenentColor.toString() || testSquarePiece == 'r' + oppenentColor.toString()) {
+                    isKingInCheck = true;
+                }
+            } else { // Diagonal path
+                if (testSquarePiece == 'q' + oppenentColor.toString() || testSquarePiece == 'b' + oppenentColor.toString()) {
+                    isKingInCheck = true;
+                }
+            }
+
+        }
+    }
+
+    // Check for pawn checks
+    var xPos = columns.indexOf(kingLocation.substr(0, 1));
+    var yPos = parseInt(kingLocation.substr(1, 1));
+
+    var squareOne = (xPos < 7 && yPos != ((pieceColor == 1) ? 8 : 1)) ? columns[xPos + 1].toString() + (yPos + ((pieceColor == 1) ? 1 : -1)).toString() : '  ';
+    var squareTwo = (xPos > 0 && yPos != ((pieceColor == 1) ? 8 : 1)) ? columns[xPos - 1].toString() + (yPos + ((pieceColor == 1) ? 1 : -1)).toString() : '  ';
+
+    if (squareOne != '  ') {
+        if (pieceLocations[squareOne] == 'p' + oppenentColor.toString()) {
+            isKingInCheck = true;
+        }
+    }
+    if (squareTwo != '  ') {
+        if (pieceLocations[squareTwo] == 'p' + oppenentColor.toString()) {
+            isKingInCheck = true;
+        }
+    }
+
+    // Check for knight checks
+    const xValues = [-2, -2, -1, -1, 1, 1, 2, 2];
+    const yValues = [1, -1, 2, -2, 2, -2, 1, -1];
+    
+    for (var i = 0; i < 8; i++) {
+        if (xPos + xValues[i] >= 0 && xPos + xValues[i] < 8 && yPos + yValues[i] > 0 && yPos + yValues[i] <= 8) {
+            if (pieceLocations[columns[xPos + xValues[i]].toString() + (yPos + yValues[i]).toString()] == 'n' + oppenentColor.toString()) {
+                isKingInCheck = true;
+            }
+        }
+    }
+
+    // Reset modified piece positions in pieceLocations object.
+    pieceLocations[oldSquare] = oldSquarePiece;
+    pieceLocations[newSquare] = newSquarePiece;
+
+    // Remove these later. *********************************************************************
+    console.log(isMoveValid);
+    console.log(!isPathBlocked);
+    console.log(!isKingInCheck);
+
+    return (isMoveValid && !isPathBlocked && !isKingInCheck); // Returns true if the move is valid and doesn't leave the king in check.
 
 }
 
@@ -180,35 +357,37 @@ function MovePiece(oldSquare, newSquare) {
     pieceLocations[oldSquare] = '  ';
 
     // Determine what piece was moved
-    var pieceName = "";
-    switch (pieceLocations[newSquare].substring(0,1)) {
+
+    var pieceName = (pieceLocations[newSquare].substr(1,1) == 1) ? "White " : "Black ";
+
+    switch (pieceLocations[newSquare].substr(0,1)) {
 
         case 'k':
-            pieceName = "King";
+            pieceName += "King";
             break;
 
         case 'q':
-            pieceName = "Queen";
+            pieceName += "Queen";
             break;
 
         case 'r':
-            pieceName = "Rook";
+            pieceName += "Rook";
             break;
 
         case 'b':
-            pieceName = "Bishop";
+            pieceName += "Bishop";
             break;
 
         case 'n':
-            pieceName = "Knight";
+            pieceName += "Knight";
             break;
 
         case 'p':
-            pieceName = "Pawn";
+            pieceName += "Pawn";
             break;
 
         default: // other unexpected value
-            console.log('Unexpected piece code in the locations object'); // TEST PURPOSES... REMOVE THIS LATER ***************************
+            console.log('MovePiece(): Unexpected piece code in the locations object'); // TEST PURPOSES... REMOVE THIS LATER ***************************
 
     }
 
@@ -217,11 +396,99 @@ function MovePiece(oldSquare, newSquare) {
     $('#' + newSquare).children('.piece').first().text(pieceName);
 }
 
-
-
+// Function runs when start game button is pressed.
 function StartGame() {
     console.log("Game Started!");
 
     // More stuff here... *******************************************************
 }
 
+// Helper function that returns the square name(s) of a piece
+function findPiece(pieceCode) {
+
+    // Initialize list of square names to an empty string
+    var squares = ""
+
+    // Loop through all squares.
+    for (const square in pieceLocations) {
+
+        // Add square name to string of results if the square contains the piece.
+        squares += (pieceLocations[square] == pieceCode) ? square + ";" : "";
+
+    }
+
+    // Return all squares with the specified piece to search for.
+    return squares;
+
+}
+
+// Calculate horizontal distance between two squares
+function CalculateXDistance(squareOne, squareTwo) {
+    return columns.indexOf(squareTwo.substr(0, 1)) - columns.indexOf(squareOne.substr(0, 1));
+}
+
+// Calculate vertical distance between two squares
+function CalculateYDistance(squareOne, squareTwo) {
+    return parseInt(squareTwo.substr(1, 1)) - parseInt(squareOne.substr(1, 1));
+}
+
+// Return the first piece encountered in a line from the starting square
+function checkPath(kingSquare, xIncrement, yIncrement) {
+
+    if (xIncrement == 0 && yIncrement == 0) {
+        return '  ';
+    }
+
+    var xPos = columns.indexOf(kingSquare.substr(0, 1));
+    var yPos = parseInt(kingSquare.substr(1, 1));
+
+    var square = '  ';
+
+    do {
+
+        xPos += xIncrement;
+        yPos += yIncrement;
+
+        if (xPos < 0 || xPos > 7 || yPos < 1 || yPos > 8) {
+            return '  ';
+        }
+
+        square = columns[xPos].toString() + yPos.toString();
+
+    } while (pieceLocations[square] == '  ');
+
+    return pieceLocations[square];
+}
+
+// Determine if there is a piece between two squares
+// Warning: Does NOT check if there is a linear path between the two squares. A non-linear path will result in an error!
+function checkPathBlocked(squareOne, squareTwo, includeSecondSquare) {
+
+    var xDistance = CalculateXDistance(squareOne, squareTwo);
+    var yDistance = CalculateYDistance(squareOne, squareTwo);
+    
+    var xPos = columns.indexOf(squareOne.substr(0, 1));
+    var yPos = parseInt(squareOne.substr(1, 1));
+
+    var xIncrement = (xDistance == 0) ? 0 : xDistance / Math.abs(xDistance);
+    var yIncrement = (yDistance == 0) ? 0 : yDistance / Math.abs(yDistance);
+
+    var square = squareOne;
+
+    do {
+
+        xPos += xIncrement;
+        yPos += yIncrement;
+
+        square = columns[xPos].toString() + yPos.toString();
+
+        if (pieceLocations[square] != '  ') {
+            if (square != squareTwo || includeSecondSquare) {
+                return true
+            }
+        }
+
+    } while (square != squareTwo);
+
+    return false;
+}
